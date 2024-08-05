fc0e241b35e2e762fe071d9c832b102c39380121
Catioro_Baiano <robalodacruzr@gmail.com>
2024-08-05 20:00:18 -0300

    Entraga Prova

---
+++ Prova/script.js
@@ -0,0 +1,8 @@
+const Nome = document.getElementById("Nome");
+const Email = document.getElementById("Email");
+const Msg_C = document.getElementById("Msg_C")
+
+function info(){
+
+    Msg_C.innerHTML = "Muito obrigado(a), senhor(a) ", Nome, ". Acabamos de enviar um e-mail para: ", Email,".";
+}
